60a62867d89612b4a9736e1c8a590251d402066a
Sandeep Gupta <gupta.sandeepcs@gmail.com>
2012-11-22 17:41:58 +0530

    Made some changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,21 @@
 #
 # Table name: users
 #
-#  id         :integer(4)      not null, primary key
-#  first_name :string(255)
-#  last_name  :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer(4)      not null, primary key
+#  first_name      :string(255)
+#  last_name       :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :first_name,:last_name,:email
+  attr_accessible :first_name,:last_name,:email,:password, :password_confirmation
+  has_secure_password
   validates :first_name, presence:true, length: {maximum: 50}
   validates :email, presence:true, uniqueness: true
+  validates :password, presence:true,length:{minimum: 6}
+  validates :password_confirmation, presence:true
   
 end
