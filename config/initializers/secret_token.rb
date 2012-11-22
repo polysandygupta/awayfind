3b358c4b520966cc4fb5867734b0c288423fc348
Sandeep Gupta <gupta.sandeepcs@gmail.com>
2012-11-22 16:01:11 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awayfind::Application.config.secret_token = 'c676e215b49d782a597732bbb6696aa1feb6250a144128f89c93913cd342920eab555a0f1bb73d901a4b506c423da6bfa049c33209ba3dc24ad17adbc0ac47d8'
